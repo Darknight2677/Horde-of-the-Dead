a8db80101bf7a3126106bc5dc56c42ff5825385a
MUKILTEO\1709575 <noahb2675@gmail.com>
2022-03-21 07:56:13 -0700

    player movement and caravan scripts

---
+++ Assets/CaravanRange.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CaravanRange : MonoBehaviour
+{
+    private Rigidbody2D rb2;
+    private Transform player;
+    public float lineOfSite;
+
+    public CaravanMove mov;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        rb2 = GetComponent<Rigidbody2D>();
+        mov.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 targ = player.position;
+        targ.z = 0f;
+
+        Vector3 objectPos = transform.position;
+        targ.x = targ.x - objectPos.x;
+        targ.y = targ.y - objectPos.y;
+
+        float distanceFromPlayer = Vector2.Distance(player.position, transform.position);
+        if (distanceFromPlayer < lineOfSite)
+        {
+            mov.enabled = true;
+        }
+        else
+        {
+            mov.enabled = false;
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(transform.position, lineOfSite);
+    }
+}
---
+++ Assets/Shooting.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour
+{
+
+    public Transform firePoint;
+    public GameObject bulletPrefab;
+
+    public float bulletForce = 20f;
+
+    //public int damage = 40;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+        Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
+        rb.AddForce(firePoint.up * bulletForce, ForceMode2D.Impulse);
+
+        //RaycastHit2D hitInfo = Physics2D.Raycast(firePoint.position, firePoint.right);
+        //if (hitInfo)
+        //{
+        //    Enemy enemy = hitInfo.transform.GetComponent<Enemy>();
+        //    if (enemy != null)
+        //    {
+        //        enemy.TakeDamage(damage);
+        //    }
+        //}
+    }
+}
