1e800cf331e737eeb568c232c2af02bfb22ac876
DESKTOP-L50R31T\noahb_fdeb1c4 <noahb2675@gmail.com>
2022-03-29 00:25:51 -0700

    tried lots of things with enemies and failed miserable in every way. there really werent any changes. Hope to find a way to target the closest object

--- Assets/EnemyCombat.cs
+++ Assets/EnemyCombat.cs
@@ -1,11 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Pathfinding;
 
 public class EnemyCombat : MonoBehaviour
 {
     //public float speed;
     private Transform NPC;
+    public float lineOfSite;
     public float attackRange;
     public float attackRate = 1f;
     private float nextAttackTime;
@@ -17,10 +19,23 @@ public class EnemyCombat : MonoBehaviour
     public int health;
     public int maxHealth = 3;
 
+    public AIDestinationSetter Destination;
+    public EnemyMovement Move;
+    public AIPath Path;
+
+    //private GameObject[] multipleNPCs;
+    //public Transform closestNPC;
+    //public bool npcContant;
+
     // Start is called before the first frame update
-    void Start()
+    public void Start()
     {
         health = maxHealth;
+        Destination.enabled = false;
+        Move.enabled = false;
+        Path.enabled = false;
+        //closestNPC = null;
+        //npcContact = false;
     }
 
     // Update is called once per frame
@@ -35,6 +50,14 @@ public class EnemyCombat : MonoBehaviour
         targ.y = targ.y - objectPos.y;
 
         float distanceFromTarget = Vector2.Distance(NPC.position, transform.position);
+
+        if (distanceFromTarget < lineOfSite)
+        {
+            Destination.enabled = true;
+            Move.enabled = true;
+            Path.enabled = true;
+        }
+
         if (distanceFromTarget < attackRange)
         {
             float angle = Mathf.Atan2(targ.y, targ.x) * Mathf.Rad2Deg;
@@ -57,6 +80,7 @@ public class EnemyCombat : MonoBehaviour
     {
         Gizmos.color = Color.green;
         Gizmos.DrawWireSphere(transform.position, attackRange);
+        Gizmos.DrawWireSphere(transform.position, lineOfSite);
     }
 
     IEnumerator Attack()
@@ -73,4 +97,23 @@ public class EnemyCombat : MonoBehaviour
             health--;
         }
     }
+
+    //void FindClosestNPC()
+    //{
+    //    multipleNPCs = GameObject.FindGameObjectsWithTag("NPC");
+    //    float closestDistance = Mathf.Infinity;
+    //    Transform trans = null;
+
+    //    foreach (GameObject go in multipleNPCs)
+    //    {
+    //        float currentDistance;
+    //        currentDistance = Vector3.Distance(transform.position, go.transform.position);
+    //        if (currentDistance < closestDistance)
+    //        {
+    //            closestDistance = currentDistance;
+    //            trans = go.transform;
+    //        }
+    //    }
+    //    return trans;
+    //}
 }
