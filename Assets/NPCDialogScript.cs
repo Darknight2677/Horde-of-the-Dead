4d70e4d4c3c621d2a95a245da326086d16e15ed9
MUKILTEO\1710275 <1710275@mukilteo.wednet.edu>
2022-03-30 10:25:39 -0700

    Dialouge

---
+++ Assets/NPCDialogScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class NPCDialogScript : MonoBehaviour
+{
+    public DialogTrigger trigger;
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player") == true)
+            trigger.StartDialogue();
+    }
+}
