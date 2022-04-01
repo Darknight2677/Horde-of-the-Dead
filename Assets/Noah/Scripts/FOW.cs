9d3e3efa92373444cdb1cea2fc1b5d57ab2dace8
MUKILTEO\1709575 <noahb2675@gmail.com>
2022-04-01 10:17:29 -0700

    no message

--- Assets/Noah/Scripts/FOW.cs
+++ Assets/Noah/Scripts/FOW.cs
@@ -1,46 +1,46 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
 
-public class FOW : MonoBehaviour
-{
-    public float viewRadius = 5;
-    public float viewAngle = 135;
-    Collider2D[] playerInRadius;
-    public LayerMask obstacleMask, playerMask;
-    public List<Transform> visiblePlayer = new List<Transform>();
+//public class FOW : MonoBehaviour
+//{
+//    public float viewRadius = 5;
+//    public float viewAngle = 135;
+//    Collider2D[] playerInRadius;
+//    public LayerMask obstacleMask, playerMask;
+//    public List<Transform> visiblePlayer = new List<Transform>();
 
-    private void FixedUpdate()
-    {
-        FindVisiblePlayer();
-    }
+//    private void FixedUpdate()
+//    {
+//        FindVisiblePlayer();
+//    }
 
-    void FindVisiblePlayer()
-    {
-        playerInRadius = Physics2D.OverlapCircleAll(transform.position, viewRadius);
+//    void FindVisiblePlayer()
+//    {
+//        playerInRadius = Physics2D.OverlapCircleAll(transform.position, viewRadius);
 
-        for (int i = 0; i < playerInRadius.Length; i++)
-        {
-            Transform player = playerInRadius[i].transform;
-            Vector2 dirPlayer = new Vector2(player.position.x - transform.position.x, player.position.y - transform.position.y);
-            if (Vector2.Angle(dirPlayer, transform.right) < viewAngle / 2)
-            {
-                float distancePlayer = Vector2.Distance(transform.position, player.position);
+//        for (int i = 0; i < playerInRadius.Length; i++)
+//        {
+//            Transform player = playerInRadius[i].transform;
+//            Vector2 dirPlayer = new Vector2(player.position.x - transform.position.x, player.position.y - transform.position.y);
+//            if (Vector2.Angle(dirPlayer, transform.right) < viewAngle / 2)
+//            {
+//                float distancePlayer = Vector2.Distance(transform.position, player.position);
 
-                if (!Physics2D.Raycast(transform.position, dirPlayer, distancePlayer, obstacleMask))
-                {
-                    visiblePlayer.Add(player);
-                }
-            }
-        }
-    }
+//                if (!Physics2D.Raycast(transform.position, dirPlayer, distancePlayer, obstacleMask))
+//                {
+//                    visiblePlayer.Add(player);
+//                }
+//            }
+//        }
+//    }
 
-    public Vector2 DirFromAngle (float angleDeg, bool global)
-    {
-        if (!global)
-        {
-            angleDeg += transform.eulerAngles.z;
-        }
-        return new Vector2(Mathf.Cos(angleDeg * Mathf.Deg2Rad), Mathf.Cos(angleDeg * Mathf.Deg2Rad));
-    }
-}
+//    public Vector2 DirFromAngle (float angleDeg, bool global)
+//    {
+//        if (!global)
+//        {
+//            angleDeg += transform.eulerAngles.z;
+//        }
+//        return new Vector2(Mathf.Cos(angleDeg * Mathf.Deg2Rad), Mathf.Sin(angleDeg * Mathf.Deg2Rad));
+//    }
+//}
