5985bd6d5828e1ef46a680439de8079f3e706440
MUKILTEO\1709575 <noahb2675@gmail.com>
2022-04-20 10:24:22 -0700

    no message

---
+++ Assets/FireAnimation.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireAnimation : MonoBehaviour
+{
+    Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = gameObject.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            //StartCoroutine(Shooting());
+            anim.SetBool("Shoot", true);
+        }
+        else
+        {
+            anim.SetBool("Shoot", false);
+        }
+    }
+
+    //IEnumerator Shooting()
+    //{
+    //    anim.SetBool("Shoot", true);
+    //    yield return new WaitForSeconds(10f);
+    //    anim.SetBool("Shoot", false);
+    //}
+}
