8c573962663594dfe507add6c4a99f894024ee99
MUKILTEO\1709575 <noahb2675@gmail.com>
2022-04-22 09:35:47 -0700

    no message

--- Assets/FireAnimation.cs
+++ Assets/FireAnimation.cs
@@ -4,26 +4,27 @@ using UnityEngine;
 
 public class FireAnimation : MonoBehaviour
 {
-    Animator anim;
+    public Animator anim;
 
     // Start is called before the first frame update
     void Start()
     {
         anim = gameObject.GetComponent<Animator>();
+        //anim.SetBool("Shoot", false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
-        {
-            //StartCoroutine(Shooting());
-            anim.SetBool("Shoot", true);
-        }
-        else
-        {
-            anim.SetBool("Shoot", false);
-        }
+        //if (Input.GetMouseButtonDown(0))
+        //{
+        //    //StartCoroutine(Shooting());
+        //    anim.SetBool("Shoot", true);
+        //}
+        //else
+        //{
+        //    anim.SetBool("Shoot", false);
+        //}
     }
 
     //IEnumerator Shooting()
--- Assets/Noah/Scripts/Bullet.cs
+++ Assets/Noah/Scripts/Bullet.cs
@@ -6,11 +6,30 @@ public class Bullet : MonoBehaviour
 {
 
     public GameObject hitEffect;
+    public GameObject objectHitEffect;
 
     void OnCollisionEnter2D(Collision2D collision)
     {
-        GameObject effect = Instantiate(hitEffect, transform.position, Quaternion.identity);
-        Destroy(effect, 0.1f);
+        if(collision.gameObject.tag == "Enemy")
+        {
+            GameObject effect = Instantiate(hitEffect, transform.position, Quaternion.identity);
+            Destroy(effect, 0.1f);
+        }
+        else if (collision.gameObject.tag == "NPC")
+        {
+            GameObject effect = Instantiate(hitEffect, transform.position, Quaternion.identity);
+            Destroy(effect, 0.1f);
+        }
+        else if (collision.gameObject.tag == "Player")
+        {
+            GameObject effect = Instantiate(hitEffect, transform.position, Quaternion.identity);
+            Destroy(effect, 0.1f);
+        }
+        else
+        {
+            GameObject effect = Instantiate(objectHitEffect, transform.position, Quaternion.identity);
+            Destroy(effect, 0.1f);
+        }
         Destroy(gameObject);
     }
 
