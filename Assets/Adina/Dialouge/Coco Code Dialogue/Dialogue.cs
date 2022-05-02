962a1e19c29d1fb952a254d44b7a0b2079ee8fce
MUKILTEO\1710275 <1710275@mukilteo.wednet.edu>
2022-05-02 08:46:06 -0700

    no message

--- Assets/Adina/Dialouge/Coco Code Dialogue/Dialogue.cs
+++ Assets/Adina/Dialouge/Coco Code Dialogue/Dialogue.cs
@@ -11,9 +11,19 @@ public class Dialogue : MonoBehaviour
     private int index;
     public float typingSpeed;
 
+    public GameObject continueButton;
+
     void Start()
     {
         StartCoroutine(Type());
+    }
+
+    void Update()
+    {
+        if(textDisplay.text == sentences[index])
+        {
+            continueButton.SetActive(true);
+        }
     }
 
     IEnumerator Type()
@@ -22,6 +32,23 @@ public class Dialogue : MonoBehaviour
         {
             textDisplay.text += letter;
             yield return new WaitForSeconds(typingSpeed);
+        }
+    }
+
+    public void NextSentence()
+    {
+        continueButton.SetActive(false);
+
+        if(index < sentences.Length - 1)
+        {
+            index++;
+            textDisplay.text = "";
+            StartCoroutine(Type());
+        }
+        else
+        {
+            textDisplay.text = "";
+            continueButton.SetActive(false);
         }
     }
 }
